d166bc613ac474eb0fee61ca890b5fbddc9fc76d
gbertand <gb@>
2025-03-21 17:24:00 -0400

    V1

---
+++ Projet.py
@@ -0,0 +1,38 @@
+# Projet 1 / Gabriel Bertrand et Marius Séguin / 2025-03-21
+# Système de Gestion de Médiathèque
+# Contexte
+"""
+Vous êtes chargé(e) de développer un système de gestion pour une médiathèque municipale.
+Cette application en ligne de commande permettra aux employés de gérer les différents types de
+médias (livres, DVDs, CDs), les utilisateurs, ainsi que les emprunts et retours.
+"""
+
+# Menu interactif
+print("Bonjour sur le site Web de Médiathèque.")  # TODO ajouter couleur
+print("Veuillez entrer le numéro de l'option souhaitée\n 0. Quitter le programme 1. Ajouter un média (livre, DVD ou CD)\n "
+      "2. Rechercher des médias selon différents critères\n 3.Afficher les détails d'un média spécifique\n "
+      "4.Ajouter un nouvel utilisateur\n 5. Emprunter un média\n 6. Retourner un média\n "
+      "7. Afficher l'historique d'un utilisateur\n 8. Consulter les statistiques de la médiathèque")
+choix_action = input("---")
+while True:
+    if choix_action == "0":
+        pass
+    elif choix_action == "1":
+        pass
+    elif choix_action == "2":
+        pass
+    elif choix_action == "3":
+        pass
+    elif choix_action == "4":
+        pass
+    elif choix_action == "5":
+        pass
+    elif choix_action == "6":
+        pass
+    elif choix_action == "7":
+        pass
+    else:
+        print("Vous avez entrer un choix inexistant ou invalide.Veuillez entrez un nombre entre 0 et 7")
+
+
+
