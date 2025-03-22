23b79e43a3cc0dea611efef79a5296aebc0e7912
gbertand <gb@>
2025-03-21 23:58:45 -0400

    V3

--- Projet.py
+++ Projet.py
@@ -1,3 +1,5 @@
+from termcolor import colored
+from abc import ABC, abstractmethod
 # Projet 1 / Gabriel Bertrand et Marius Séguin / 2025-03-21
 # Système de Gestion de Médiathèque
 # Contexte
@@ -7,17 +9,73 @@ Cette application en ligne de commande permettra aux employés de gérer les dif
 médias (livres, DVDs, CDs), les utilisateurs, ainsi que les emprunts et retours.
 """
 
+
+class Mediatheque:
+    """Classe sevant à la gestion globale du programme"""
+    pass
+
+class Empruntable(ABC):
+    pass
+
+class Medias(ABC):
+    def __init__(self, identifiant, titre, annee_parution, genre):
+        self.__identifiant = identifiant
+        self.__titre = titre
+        self.__annee_parution = annee_parution
+        self.__genre = genre
+
+    def get_identifiant(self):
+        return self.__identifiant
+
+    def get_titre(self):
+        return self.__titre
+
+    def get_annee_parution(self):
+        return self.__annee_parution
+
+    def get_genre(self):
+        return self.__genre
+
+    @abstractmethod
+    def afficher_info(self):
+        pass
+    pass
+
+class Livre(Medias, Empruntable):    #TODO vérifier si Empruntable est implanter
+    def __init__(self, identifiant, titre, annee_parution, genre, auteur, nombre_pages):
+        super().__init__(identifiant, titre, annee_parution, genre)
+        self.__auteur = auteur
+        self.__nombre_pages = nombre_pages
+
+    def afficher_info(self):
+        return f"Livre: {self.titre} de {self.auteur}, Genre: {self.genre}, Pages: {self.__nombre_pages}"  #TODO get nombres pages avant et finir la ligne
+    pass
+
+class DVD(Medias, Empruntable):
+    def __init__(self,realisateur, duree_minutes):
+        self.__realisateur = realisateur
+        self.__duree_minutes = duree_minutes
+    pass
+
+class CD(Medias, Empruntable):
+    def __init__(self,artiste, nombre_pistes):
+        self.__artiste = artiste
+        self.__nombre_pistes = nombre_pistes
+    pass
+
+
 # Menu interactif
 print("Bonjour sur le site Web de Médiathèque.")  # TODO ajouter couleur
-print("Veuillez entrer le numéro de l'option souhaitée\n 0. Quitter le programme 1. Ajouter un média (livre, DVD ou CD)\n "
-      "2. Rechercher des médias selon différents critères\n 3.Afficher les détails d'un média spécifique\n "
-      "4.Ajouter un nouvel utilisateur\n 5. Emprunter un média\n 6. Retourner un média\n "
-      "7. Afficher l'historique d'un utilisateur\n 8. Consulter les statistiques de la médiathèque")
-
-choix_action = input("---")
 while True:
+    print("Veuillez entrer le numéro de l'option souhaitée\n 0. Quitter le programme 1. Ajouter un média (livre, DVD ou CD)\n "
+        "2. Rechercher des médias (par titre, genre, année, disponibilité)\n 3.Afficher les détails d'un média spécifique\n "
+        "4.Ajouter un nouvel utilisateur\n 5. Emprunter un média\n 6. Retourner un média\n "
+        "7. Afficher l'historique d'un utilisateur\n 8. Consulter les statistiques de la médiathèque")
+
+    choix_action = input("---")
     if choix_action == "0":
-        pass
+        print("Il a été un plaisir de vous servir.Aure voir!")
+        break
     elif choix_action == "1":
         pass
     elif choix_action == "2":
@@ -33,7 +91,7 @@ while True:
     elif choix_action == "7":
         pass
     else:
-        print("Vous avez entrer un choix inexistant ou invalide.Veuillez entrez un nombre entre 0 et 7")
+        print(colored("Vous avez entrer un choix inexistant ou invalide.Veuillez entrez un nombre entre 0 et 7", "red"))
 
 
 
